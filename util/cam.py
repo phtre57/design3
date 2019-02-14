3ec80ebd4d1bad390525f39ef83a17371b825baa
Louis-David Lacerte <ldlacerte@hotmail.com>
2019-02-14 14:50:24 -0500

    changed shapeDetector detect breaking change

--- util/cam.py
+++ util/cam.py
@@ -3,7 +3,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 ret,img = cap.read()
 cap.release()
-cv2.imwrite('globalmonde9.jpg', img)
+cv2.imwrite('zonedep2.jpg', img)
 cv2.imshow('ok', img)
 
 cv2.waitKey()
