2d1f2118fb498e0e5932099fefd0369d1c6fb7b1
Ioannis32 <isar.1@hotmail.com>
2019-02-22 12:53:48 -0500

    Added basic UI frame

--- ui/web_client/src/App.js
+++ ui/web_client/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper';
+
 import openSocket from 'socket.io-client';
 
 class App extends Component {
@@ -17,8 +23,8 @@ class App extends Component {
     });
   }
   
-  alloSocket = () => {
-    this.state.socket.emit("event", "ok");
+  startSignal = () => {
+    this.state.socket.emit("start", "start");
   }
 
   render() {
@@ -29,13 +35,79 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <button onClick={this.alloSocket}>ALLO</button>
-          <img src={imgR} width="200" alt="logo"/>
-        </header>
+          <AppBar position="static" color="primary">
+              <Toolbar>
+                  <Typography variant="h6" color="inherit">
+                      Station de base
+                  </Typography>
+              </Toolbar>
+          </AppBar>
+          <div style={mainPaper}>
+          <Paper elevation={15}>
+          <div style={mainDock}>
+              <Paper elevation={4}>
+                  <span style={textZone}>Info Code QR</span>
+                  <Button variant="contained" color="primary" style={buttonStyle} onClick={this.startSignal}>
+                      Start
+                  </Button>
+                  <span style={textZone}>Chronographe</span>
+              </Paper>
+          </div>
+          <div style={container}>
+              <Paper elevation={4} style={imageStyle}>
+                  <span style={textZone}>Courant et</span>
+                  <span style={textZone}>tension condensateur</span>
+              </Paper>
+              <Paper elevation={4} style={imageStyle}>
+                  <span style={textZone}>États présent du robot</span>
+              </Paper>
+          </div>
+          <div style={container}>
+              <Paper elevation={4} style={imageStyle}>
+                  <img src={imgR} width="200" alt="logo"/>
+              </Paper>
+              <Paper elevation={4} style={imageStyle}>
+                  <img src={imgR} width="200" alt="logo"/>
+              </Paper>
+          </div>
+          </Paper>
+          </div>
       </div>
     );
   }
 }
+
+const imageStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  margin: '10px',
+  padding: '10px'
+};
+
+const container = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+};
+
+const mainPaper = {
+  marginLeft: '100px',
+  marginRight: '100px',
+  marginTop: '10px',
+};
+
+const mainDock = {
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '10px'
+};
+
+const buttonStyle = {
+  margin: '10px'
+};
+
+const textZone = {
+  margin: '10px 50px 10px 50px'
+};
 
 export default App;
