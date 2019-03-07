c3061900d97f6f450b14d30a2afd5a55dbd0e8d7
Philippe <phtre57@gmail.com>
2019-03-07 12:37:19 -0500

    change large test to add robot detector

--- domain/LargeTest/MovingRobotTest.py
+++ domain/LargeTest/MovingRobotTest.py
@@ -4,6 +4,7 @@ from domain.pathfinding.PathSmoother import PathSmoother
 from domain.image_path_analysis.PixelToXYCoordinatesConverter import *
 from domain.image_path_analysis.ImageToGridConverter import *
 from infrastructure.communication_pi import comm_pi
+from domain.image_path_analysis.RobotDetector import RobotDetector
 
 #this test do not reassess position of robot
 def test_main_loop_move_robot():
@@ -25,7 +26,10 @@ def test_main_loop_move_robot():
 
     start = time.time()
 
-    grid_converter = ImageToGridConverter(img, LENGTH - 1, HEIGHT - 1)
+    robot_detector = RobotDetector(img)
+    x_start, y_start = robot_detector.find_center_of_robot()
+
+    grid_converter = ImageToGridConverter(img, x_start, y_start, 50, 50)
 
     astar = Astar(grid_converter.grid, HEIGHT, LENGTH)
 
