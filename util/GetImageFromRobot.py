0d59825b28f8ebfb8411b051c0c0a244583e2c2d
Philippe <phtre57@gmail.com>
2019-03-25 14:00:19 -0400

    added util to get image from robot

---
+++ util/GetImageFromRobot.py
@@ -0,0 +1,20 @@
+import time
+import cv2
+from infrastructure.communication_pi.comm_pi import *
+
+
+def take_image(comm_pi):
+    image = comm_pi.getImage()
+    cv2.imshow("test", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imwrite("piece.jpg", image)
+
+
+comm_pi = Communication_pi()
+comm_pi.connectToPi()
+comm_pi.changeServoVert('6000')
+comm_pi.changeServoHori('2000')
+#take_image(comm_pi)
+comm_pi.disconnectFromPi()
